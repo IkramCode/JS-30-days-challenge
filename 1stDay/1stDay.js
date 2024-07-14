a77fbe7f99e44e47efc4efb96dae7da0066e6c3a
IkramCode <moikramcoder@gmail.com>
2024-07-14 14:25:28 +0500

    some error in repo

---
+++ 1stDay/1stDay.js
@@ -0,0 +1,59 @@
+var number = 33;
+console.log(number);
+
+let string = "Mo-Ikram";
+console.log(string);
+
+const bool = true;
+console.log(bool);
+
+let anotherNum = 56;
+let anotherStr = "UL HAQ";
+let anotherBol = false;
+let obj = {
+  name: "Ikram ul haq",
+  coder: true,
+};
+let arr = ["JS", "Tailwind", "Node", "React"];
+let array = Array.from(arr)
+let x;
+
+console.log(typeof anotherNum);
+console.log(typeof anotherStr);
+console.log(typeof anotherBol);
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof x);
+
+var language = "Js";
+console.log(language);
+
+var language = "python";
+console.log(language);
+
+let library = "node";
+console.log(library);
+library = "tailwind"
+console.log(library);
+
+// const bool = false
+console.log(bool);
+
+console.log(`Value of anotherNum is ${anotherNum} and typeof anotherNum is`, typeof anotherNum); 
+console.log(`Value of anotherStr is ${anotherStr} and typeof anotherStr is`, typeof anotherStr); 
+console.log(`Value of anotherBol is ${anotherBol} and typeof anotherBol is`, typeof anotherBol); 
+console.log(`Value of obj is ${JSON.stringify(obj)} and typeof obj is`, typeof obj); 
+console.log(`Value of arr is ${arr} and typeof arr is`, typeof arr); 
+console.log(`Value of array is ${array} and typeof array is`, typeof array); 
+console.log(`Is arr an array?`, Array.isArray(arr)); 
+console.log(`Is array an array?`, Array.isArray(array)); 
+console.log(`Value of x is ${x} and typeof x is`, typeof x); 
+
+
+let framework = "react"
+console.log(framework);
+const libry = "tailwind"
+// libry = "node" not possible due to immutability
+framework = "redux"
+console.log(framework);
+console.log(libry);
---
+++ README.md
@@ -0,0 +1,2 @@
+# 30days-JS-challenge
+30 days Javascript challenge to sharpen my JavaScript expertise
