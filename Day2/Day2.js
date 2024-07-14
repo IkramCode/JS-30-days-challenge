ce2bf65ef0509aff910837c529dbaef02fbeff20
IkramCode <moikramcoder@gmail.com>
2024-07-14 15:55:28 +0500

    Done with day 2

---
+++ Day2/Day2.js
@@ -0,0 +1,73 @@
+let variable = "1";
+
+let addNum1 = 5;
+let addNum2 = 6;
+// console.log(addNum1 + addNum2);
+
+let subNum1 = 1;
+let subNum2 = 8;
+// console.log(subNum2 - subNum1);
+// console.log(typeof addNum1);
+// console.log(typeof subNum2);
+
+let mulNum1 = 36;
+let mulNum2 = 2;
+// console.log(mulNum1 * mulNum2);
+
+let divNum1 = 108;
+let divNum2 = 12;
+// console.log(divNum1 / divNum2);
+
+let remNum1 = 72;
+let remNum2 = 7;
+// console.log(`Remainder when ${remNum1} is divided by ${remNum2} is ${remNum1 % remNum2} `);
+
+// console.log(addNum1 += variable);
+// console.log(subNum2 -= variable);
+// console.log(typeof addNum1);
+// console.log(typeof subNum2);
+
+let typeNum1 = 100;
+let typeNum2 = 100;
+let typeStr = "string";
+
+// console.log(addNum1 > addNum2);
+// console.log(addNum1 < addNum2);
+// console.log(typeNum1 <= typeNum2);
+// console.log(typeNum1 >= typeNum2);
+// console.log(typeNum1 == typeNum2);
+// console.log(typeNum1 === typeNum2);
+// console.log(typeNum1 === typeStr);
+
+// console.log(typeNum1 == typeNum2 && addNum1 < addNum2);
+// console.log(typeNum1 == typeNum2 || addNum1 < addNum2);
+// console.log(typeNum1 != typeNum2);
+
+let num = 59;
+posOrneg = num > 0 ? "postive" : "negative";
+// console.log(posOrneg);
+
+let num1 = 10;
+let num2 = 9;
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let remainder = num1 % num2;
+
+// console.log(`Addition of ${num1} and ${num2} is ${addition}`);
+// console.log(`subtraction of ${num1} and ${num2} is ${subtraction}`);
+// console.log(`multiplication of ${num1} and ${num2} is ${multiplication}`);
+// console.log(`division of ${num1} and ${num2} is ${division}`);
+// console.log(`remainder of ${num1} and ${num2} is ${remainder}`);
+
+// console.log(`is ${num1} greater than ${num2} : ${num1 > num2}`);
+// console.log(`is ${num1} smaller than ${num2} : ${num1 < num2}`);
+// console.log(`is ${num1} greater or equal to ${num2} : ${num1 >= num2}`);
+// console.log(`is ${num1} smaller or equal ${num2} : ${num1 <= num2}`);
+// console.log(`${num1} is not equal to ${num2}  : ${num1 == num2}`);
+// console.log(
+//   `${num1} and ${num2} have equal datatypes and are equal  : ${num1 === num2}`
+// );
+
